77d9181f63376f8abd1166c677a37491d7b0c451
Adriatik Gashi <adriatik.gashi@stud.h-da.de>
2021-06-13 20:05:29 +0200

    Rename last rewards to state-value V

--- src/alorithms/abstract_iteration_strategy.py
+++ src/alorithms/abstract_iteration_strategy.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 
-from src.board.board_state import BoardState
 from src.board.board_service import BoardCalculationService
 from src.board.visualization.console_printer import pretty_print_to_console
 
@@ -9,20 +8,20 @@ class IterationStrategy(ABC):
 
     def __init__(self, board_layout) -> None:
         super().__init__()
-        self._board_layout = BoardState(board_layout)
+        self._board_layout = board_layout
         self._board_service = BoardCalculationService(self._board_layout)
-        self._last_rewards = self._board_service.init_rewards()
+        self.V = self._board_service.init_state_values()
 
     def reset_rewards(self):
-        self._last_rewards = self._board_service.init_rewards()
+        self.V = self._board_service.init_state_values()
 
     def run_iteration(self):
         self.run_iteration_impl()
-        pretty_print_to_console(self._last_rewards)
+        pretty_print_to_console(self.V)
 
     @abstractmethod
     def run_iteration_impl(self):
         pass
 
     def show_loss(self):
-        return self._last_rewards
+        return self.V
