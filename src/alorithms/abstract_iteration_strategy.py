f00277ded96deb25dc24152b002af1a35235cd28
Adriatik Gashi <adriatik.gashi@stud.h-da.de>
2021-06-13 18:11:26 +0200

    Extracted print to console method for strategies

--- src/alorithms/abstract_iteration_strategy.py
+++ src/alorithms/abstract_iteration_strategy.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 from src.board.board_state import BoardState
 from src.board.board_service import BoardCalculationService
+from src.board.visualization.console_printer import pretty_print_to_console
 
 
 class IterationStrategy(ABC):
@@ -15,10 +16,13 @@ class IterationStrategy(ABC):
     def reset_rewards(self):
         self._last_rewards = self._board_service.init_rewards()
 
-    @abstractmethod
     def run_iteration(self):
-        pass
+        self.run_iteration_impl()
+        pretty_print_to_console(self._last_rewards)
 
     @abstractmethod
-    def show_loss(self):
+    def run_iteration_impl(self):
         pass
+
+    def show_loss(self):
+        return self._last_rewards
