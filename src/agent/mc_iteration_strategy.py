0a03f9b11d91d8fa46fbc347858efec736f940c5
Adriatik Gashi <adriatik.gashi@stud.h-da.de>
2021-06-14 19:08:10 +0200

    Improve MC docstring

--- src/agent/mc_iteration_strategy.py
+++ src/agent/mc_iteration_strategy.py
@@ -7,12 +7,20 @@ class McIterationStrategy(IterationStrategy):
         This is the Monte Carlo strategy
         There is no domain knowledge available
 
-        Implement an algorithm that runs episodes starting from a random position, summing up the rewards
-        Then, update the policy after each episode (not after each step!)
+        Two options for implementation:
+
+            * Implement an algorithm that runs episodes starting from a random position, summing up the rewards
+              after each episode. Then, update the policy after each episode (not after each step!)
+
+            * Implement an algorithm that starts always from the same position, but with epsilon-greedy to
+              reach every state, summing up the rewards after each episode. Then, update the policy after each episode
+              (not after each step!)
+
+        Watch out for infinite loops! Set a timeout (e.g. 100 steps per episode) to avoid running into an infinite loop
+        in the grid world! (e.g. |right|left| -> infinite loop)
     """
 
     def run_iteration_impl(self, iterations=100):
-
         pass
 
     def state_value(self, state):
