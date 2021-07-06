9b65dfbb8e72fa638ea7193130c265584e7c9712
Adriatik Gashi <adriatik.gashi@stud.h-da.de>
2021-07-06 22:45:07 +0200

    adjust main

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.agent.solutions.td_iteration_strategy import TdIterationStrategy
 from src.env.environment import Environment
 from src.visualization.drl_board import DrlBoard
 
-grid_world_layout = [['a', 0, 0, 0, 0, 0, 0, 0, 0],
+grid_world_layout = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 1, 1, 0, 1, 0, 1, 1, 0],
                      [0, 0, 1, 0, 0, 0, 1, 0, 0],
                      [0, 1, 1, 1, 1, 0, 1, 0, 0],
